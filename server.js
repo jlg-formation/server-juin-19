89bfe3ea9b3c700fc3f89e192ec3ded5328fb326
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-06-11 12:04:35 +0200

    ok

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const serveIndex = require('serve-index');
+
+
 const app = express();
 
 app.use(function (req, res, next) {
@@ -6,7 +9,9 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.use(express.static('.'));
+const dir = '../exo/dist/exo';
+app.use(express.static(dir));
+app.use(serveIndex(dir, { icons: true }));
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
